1ed60b569d46e0a7b2305c35dade3db5398a67fe
nickdude <sharmamanish70081@gmail.com>
2026-01-16 15:26:14 +0530

    insight

--- src/components/InsightCard.js
+++ src/components/InsightCard.js
@@ -6,7 +6,24 @@ export default function InsightCard({img, title, description, width="w-[35%]"}) 
   return (
     <div className={`${width} flex flex-col`}>
         <Image src={img} alt={title} width={420} height={300} className="object-cover"/>
-        <h2 className="font-poppins font-normal text-[18px] md:text-[28px] my-2">{title}</h2>
+          <div className="relative z-20 flex justify-start w-full pt-5">
+                <h2 className="font-poppins font-normal text-[18px] md:text-[28px] my-2">{title}</h2>
+                 <div
+                  className="
+                    p-2 rounded-md
+                    transition-colors duration-200
+                    group-hover:bg-white
+                  "
+                >
+                  <Image
+                    src="/assets/icons/red_upper_arrow.svg"
+                    alt="service arrow"
+                    width={12}
+                    height={12}
+                  />
+                </div>
+          </div>
+
         <p className="font-avenir font-light text-[14px] md:text-sm">{description}</p>
     </div>
   );
--- src/components/OurInsights.js
+++ src/components/OurInsights.js
@@ -29,16 +29,29 @@ export default function OurInsights() {
             img: "/assets/insights/insight3.png",
             title: "Maximizing Small Spaces",
             description: "Get insights into designing compact spaces without compromising style or comfort, perfect for city living and smaller homes."
+        },
+        {
+            img: "/assets/insights/insight1.png",
+            title: "Maximizing Small Spaces",
+            description: "Get insights into designing compact spaces without compromising style or comfort, perfect for city living and smaller homes."
         }
     ]
   return (
-    <section className={`w-full py-10 md:py-20 md:px-16 flex flex-col items-center justify-center gap-3 md:gap-10 ${theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}>
+    <section className={`w-full py-10 md:py-20 md:pl-16 flex flex-col items-center justify-center gap-3 md:gap-10 ${theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}>
       <h1 className="font-poppins font-medium text-[28px] md:text-6xl leading-[57px] tracking-normal">Our Insights</h1>
       <Button label="VIEW MORE" theme={theme} />
-      <div className="flex items-center justify-between w-full gap-10">
-        {insights.map((insight, index) => (
-            <InsightCard key={index} img={insight.img} title={insight.title} description={insight.description}/>
-        ))} 
+       <div className="w-full overflow-x-auto scrollbar-hide">
+        <div className="flex gap-10 min-w-max">
+          {insights.map((insight, index) => (
+            <InsightCard
+              key={index}
+              img={insight.img}
+              title={insight.title}
+              description={insight.description}
+              width="w-[430px]"
+            />
+          ))}
+        </div>
       </div>
     </section>
   );

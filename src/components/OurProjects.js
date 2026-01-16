73ea0ef9cef4fa4aa75c4a0a56603fe18d15f1a4
nickdude <sharmamanish70081@gmail.com>
2026-01-16 17:25:03 +0530

    service

--- src/components/OurProjects.js
+++ src/components/OurProjects.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Button from "./Button";
-import ProjectHoverCard from "./ProjectHoverCard";
+import ProjectCardSample from "./ProjectCardSample";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
@@ -15,37 +15,33 @@ export default function OurProjects({}) {
 
   return (
     <section className={`w-full py-20 md:py-40 ${theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}>
-        <div className="flex flex-col md:flex-row items-center gap-1">
+        <div className="flex flex-col md:flex-row items-center gap-1 overflow-x-auto scrollbar-hide">
                     <div className="w-full md:w-80 flex flex-col gap-3 md:gap-6 items-center md:items-start md:mx-16 mb-10 md:mb-0">
                         <h1 className="font-poppins font-medium text-[28px] md:w-80 md:text-[64px] leading-[68px]">Some of Our Projects</h1>
                         <p className="font-avenir font-light text-center w-70 md:w-80 md:text-left text-[16px] md:text-lg text-gray-600 leading-[25px]">Stay updated with our latest projects, press features, and industry insights.</p>
                         <Button label="VIEW ALL " theme="light"/>
                     </div>
-                    <div className="flex md:items-center md:relative overflow-x-hidden md:w-full md:h-[520px]">
-                          <ProjectHoverCard
-                          title="Bridge+"
-                          image="/assets/blueprints/blueprint3.png"
-                          city="Chennai"
-                          location="Chennai"
-                          area="1,00,000 sqft"
-                          service="General Contracting"
-                        />
-                        <ProjectHoverCard
-                          title="Bridge+"
-                          image="/assets/blueprints/blueprint3.png"
-                          city="Chennai"
-                          location="Chennai"
-                          area="1,00,000 sqft"
-                          service="General Contracting"
-                        />
-                        <ProjectHoverCard
-                          title="Bridge+"
-                          image="/assets/blueprints/blueprint3.png"
-                          city="Chennai"
-                          location="Chennai"
-                          area="1,00,000 sqft"
-                          service="General Contracting"
-                        />
+                    <div className="flex md:items-center md:relative md:h-[520px] gap-3">
+                        <ProjectCardSample
+                                  image="/assets/blueprints/blueprint3.png"
+                                  title="Bridge+"
+                                  location="Chennai"
+                                />
+                        <ProjectCardSample
+                                  image="/assets/blueprints/blueprint3.png"
+                                  title="Bridge+"
+                                  location="Chennai"
+                                />
+                        <ProjectCardSample
+                                  image="/assets/blueprints/blueprint3.png"
+                                  title="Bridge+"
+                                  location="Chennai"
+                                />
+                        <ProjectCardSample
+                                  image="/assets/blueprints/blueprint3.png"
+                                  title="Bridge+"
+                                  location="Chennai"
+                                />
                     </div>
                     
         </div>

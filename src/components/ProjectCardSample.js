e4667896dac081edfc3abcac95a74922c2978a0a
nickdude <sharmamanish70081@gmail.com>
2026-01-26 15:16:20 +0530

    rs sign

--- src/components/ProjectCardSample.js
+++ src/components/ProjectCardSample.js
@@ -9,23 +9,21 @@ export default function ProjectCardSample() {
       <div className="overlay overlay-default">
         <div className="title-box">
           <h2 className="font-poppins text-[62px]">Bridge+</h2>
-₹
           <div className="arrow-overlay">
               <img src="/assets/icons/up_arrow.svg" alt="Project Image" />
           </div>
-
           <p className="font-bricolage font-semibold text-[16px]">Chennai</p>
         </div>
       </div>
 
       <div className="overlay overlay-hover">
-        <div className="hover-box">
-          <p>Location: Chennai</p>
+        <div className="hover-box font-roboto text-lg">
+          <p>Location: Chennais</p>
           <p className="from-bottom">Area: 1,00,000 sq.ft.</p>
           <p>Service: General Contracting</p>
 
           <div className="cta from-bottom">
-            <span>View Case Study</span>
+            <span className="font-bricolage">View Case Study</span>
           </div>
         </div>
       </div>

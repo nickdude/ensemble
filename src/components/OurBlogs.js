22de342549285ad079e39c193b4bc32711e708de
nickdude <sharmamanish70081@gmail.com>
2026-02-14 17:21:25 +0530

    border

--- src/components/OurBlogs.js
+++ src/components/OurBlogs.js
@@ -12,7 +12,7 @@ export default function OurBlogs({ blogsCards }) {
 
     if (!mounted) return null;
   return (
-    <section className={`px-4 py-10 md:py-20 md:px-16 flex flex-col gap-4 border-b border-gray-300 ${theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}>
+    <section className={`px-4 py-10 md:py-20 md:px-16 flex flex-col gap-4 ${theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-10">
               {blogsCards.map((blog, index) => (
                   <InsightCard key={index} slug={blog.slug} img={blog.img} title={blog.title} description={blog.description} width="w-full"/>

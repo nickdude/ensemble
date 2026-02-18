c3ce07f3ecfa0b4928fe542fc739373c697abeaf
nickdude <sharmamanish70081@gmail.com>
2026-02-18 22:05:06 +0530

    address

--- src/components/InquiryForm.js
+++ src/components/InquiryForm.js
@@ -17,29 +17,20 @@ export default function InquiryForm(){
     if (!mounted) return null;
     return(
         <section className={`py-20 px-4 md:px-16  ${theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}>
-            <div className="flex flex-col justify-center md:grid md:grid-cols-[25%25%50%]">
+            <div className="flex flex-col justify-center md:grid md:grid-cols-[50%50%]">
                 <div className="font-inter flex items-center md:items-start flex-col gap-4 text-center md:text-left">
                     <h1 className="font-semibold text-[32px]">{offices[0].section}</h1>
-                    {offices[0].locations.map((location, index) => (
-                        <div key={index}>
-                            <h2 className="font-semibold text-[15px] my-2">
-                                {location.name}
-                            </h2>
-                            <p className="font-normal text-[16px]">{location.address}</p>
-                        </div>
-                    ))}
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+                        {offices[0].locations.map((location, index) => (
+                            <div key={index}>
+                                <h2 className="font-semibold text-[15px] my-2">
+                                    {location.name}
+                                </h2>
+                                <p className="font-normal text-[16px]">{location.address}</p>
+                            </div>
+                        ))}
+                    </div>
                     <p>{offices[0].email}</p>
-                </div>
-                 <div className="font-inter flex items-center md:items-start flex-col gap-4 text-center md:text-left md:pl-6">
-                    <h1 className="font-semibold text-[32px]">{offices[1].section || "\u00A0"}</h1>
-                    {offices[1].locations.map((location, index) => (
-                        <div key={index}>
-                            <h2 className="font-semibold text-[15px] my-2">
-                                {location.name}
-                            </h2>
-                            <p className="font-normal text-[16px]">{location.address}</p>
-                        </div>
-                    ))}
                 </div>
                  <div className="md:px-20 md:border-l md:border-[#f6f6f6]">
                     <h1 className="font-semibold text-[32px] mb-6">{inquiryForm.title}</h1>
--- src/data/home/contactData.js
+++ src/data/home/contactData.js
@@ -12,25 +12,33 @@ export const contactData = {
                     address: "Level 5, SB Tower, 1A/1 Sector, 16 A - Film City, Noida - 201301"
                 },
                 {
-                    name: "Other Offices",
-                    address: "Hyderabad & Chennai"
-                }
-            ],
-            email: "info@ensemble.co.in"
-        },
-        {
-            section: "",
-            locations: [
-                {
                     name: "Bengaluru",
                     address: "Prestige Ridge, 3rd Floor, 24/25, Domlur Service Rd, Above HDFC Bank, Domlur, Bengaluru - 560071"
                 },
                 {
                     name: "Pune",
                     address: "37 Ashapuri, 270/1, Nehru Marg, Pune - 411002"
+                },
+                {
+                    name: "Other Offices",
+                    address: "Hyderabad & Chennai"
                 }
-            ]
-        }
+            ],
+            email: "info@ensemble.co.in"
+        },
+        // {
+        //     section: "",
+        //     locations: [
+        //         {
+        //             name: "Bengaluru",
+        //             address: "Prestige Ridge, 3rd Floor, 24/25, Domlur Service Rd, Above HDFC Bank, Domlur, Bengaluru - 560071"
+        //         },
+        //         {
+        //             name: "Pune",
+        //             address: "37 Ashapuri, 270/1, Nehru Marg, Pune - 411002"
+        //         }
+        //     ]
+        // }
     ],
 
     inquiryForm: {

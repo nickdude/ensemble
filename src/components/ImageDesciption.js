1cd425bd3c96d00f403921ac8dd2062c55f74d7a
nickdude <sharmamanish70081@gmail.com>
2026-02-18 21:41:46 +0530

    mobile view image

--- src/components/ImageDesciption.js
+++ src/components/ImageDesciption.js
@@ -18,8 +18,8 @@ export default function ImageDescription({ title, description, image, right = fa
         right ? "flex-col md:flex-row-reverse" : "flex-col md:flex-row"
       }`}
     >
-      {/* Image */}
-      <div className="relative w-full md:w-1/2 h-[590px]">
+      {/* Image (desktop) */}
+      <div className="hidden md:block relative w-full md:w-1/2 md:h-[590px]">
         <Image
           src={image}
           alt={title}
@@ -31,13 +31,24 @@ export default function ImageDescription({ title, description, image, right = fa
 
       {/* Text */}
       <div
-        className={`w-full md:w-1/2 h-[590px] flex flex-col justify-center gap-5 ${
+        className={`w-full md:w-1/2 h-auto md:h-[590px] flex flex-col justify-center gap-5 ${
           right ? "pr-0 md:pr-16" : "pl-0 md:pl-16"
         }`}
       >
         <h1 className="font-roboto font-semibold text-[32px]">
           {title}
         </h1>
+
+        {/* Image (mobile) */}
+        <div className="block md:hidden relative w-full h-[320px]">
+          <Image
+            src={image}
+            alt={title}
+            fill
+            className="object-cover rounded-lg"
+            priority
+          />
+        </div>
 
         <p className="font-roboto font-light text-2xl text-gray-600 leading-relaxed">
           {description}
